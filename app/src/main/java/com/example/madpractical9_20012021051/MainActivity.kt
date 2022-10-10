77d080d20eb2c655f06781dd77cf9a08c7e180d9
sidhhii <siddhitoliya20@gnu.ac.in>
2022-10-10 12:41:47 +0530

    update

--- app/src/main/java/com/example/madpractical9_20012021051/MainActivity.kt
+++ app/src/main/java/com/example/madpractical9_20012021051/MainActivity.kt
@@ -8,23 +8,23 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
-    val SMS_PERMISSION_CODE=110
-}
-    private fun requestSMSPermission() {
-        (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_SMS)) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_SMS))
-
-        }
-            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.READ_SMS,
-            Manifest. permission. SEND_SMS ,
-            Manifest. permission. RECEIVE_SMS),
-            )
-        }
-
-    private val isSMSReadPermission:Boolean
-    get() = ContextCompat.checkSelfPermission(this , Manifest.permission.READ_SMS)==PackageManager.PERMISSION_GRANTED
-    private val isSMSWritePermission:Boolean
-    get() = ContextCompat.checkSelfPermission(this , Manifest.permission.SEND_SMS)==PackageManager.PERMISSION_GRANTED
+//    val SMS_PERMISSION_CODE=110
+//}
+//    private fun requestSMSPermission() {
+//        (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_SMS)) {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_SMS))
+//
+//        }
+//            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.READ_SMS,
+//            Manifest. permission. SEND_SMS ,
+//            Manifest. permission. RECEIVE_SMS),
+//            )
+//        }
+//
+//    private val isSMSReadPermission:Boolean
+//    get() = ContextCompat.checkSelfPermission(this , Manifest.permission.READ_SMS)==PackageManager.PERMISSION_GRANTED
+//    private val isSMSWritePermission:Boolean
+//    get() = ContextCompat.checkSelfPermission(this , Manifest.permission.SEND_SMS)==PackageManager.PERMISSION_GRANTED
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
